1955703c058d39b186e6f153f7f16d670971e1b8
VASHISHT-RAHUL <cs18d006@smail.iitm.ac.in>
2019-12-29 14:18:24 +0530

    Training_Pretrained_models

---
+++ 1_Attention_CIFAR_Mosaic/9_Exp_8_Pretrained_model/model_lr1/Pretrained_CIFAR10_mosaic_data_lr1.py
@@ -0,0 +1,590 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+import pandas as pd
+import numpy as np
+
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import torch.optim as optim
+#from matplotlib import pyplot as plt
+
+import copy
+import pickle
+
+# Ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+
+# In[2]:
+
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
+
+# In[3]:
+
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=256, shuffle=True)
+testloader = torch.utils.data.DataLoader(testset, batch_size=256, shuffle=False)
+
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+foreground_classes = {'plane', 'car', 'bird'}
+
+background_classes = {'cat', 'deer', 'dog', 'frog', 'horse','ship', 'truck'}
+
+# print(type(foreground_classes))
+
+
+# In[4]:
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net,self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120,10)
+    def forward(self,x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1,16*5*5)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        return x
+        
+
+
+# In[5]:
+
+
+net = Net()
+net.to(device)
+
+
+# In[6]:
+
+
+images,labels =  iter(trainloader).next()
+
+
+# In[7]:
+
+
+# net(images).shape
+
+
+# In[8]:
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+
+
+# In[9]:
+
+
+nos_epochs = 100
+running_acc = 0
+cnt=0
+for epoch in range(nos_epochs):
+    print('[%d, %5d] acc: %.3f' %(epoch + 1, cnt + 1, running_acc/60000))
+    running_loss = 0.0
+    running_acc = 0
+    cnt=0
+    
+    for i, data in enumerate(trainloader):
+        images,labels = data
+        images,labels = images.to(device), labels.to(device)
+        
+        optimizer.zero_grad()
+        
+        outputs = net(images)
+        
+        _, predicted = torch.max(outputs.data, 1)
+        
+        loss = criterion(outputs, labels) 
+        loss.backward()
+        
+        optimizer.step()
+        
+        running_loss += loss.item()
+        running_acc  += sum(predicted.cpu().numpy()== labels.cpu().numpy())
+        if cnt % 40 == 39:    # print every 6 mini-batches
+            print('[%d, %5d] loss: %.3f' %(epoch + 1, cnt + 1, running_loss / 40))
+            running_loss = 0.0
+            torch.save(net.state_dict(),"weight_pretrained1/net_epoch"+str(epoch)+".pt")
+        
+        cnt +=1 
+print("Finished Training")
+torch.save(net.state_dict(),"weight_pretrained1/net_epoch"+str(epoch)+".pt")
+        
+        
+
+
+# In[10]:
+
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=10, shuffle=True)
+testloader = torch.utils.data.DataLoader(testset, batch_size=10, shuffle=False)
+
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+foreground_classes = {'plane', 'car', 'bird'}
+
+background_classes = {'cat', 'deer', 'dog', 'frog', 'horse','ship', 'truck'}
+
+# print(type(foreground_classes))
+
+
+# In[11]:
+
+
+dataiter = iter(trainloader)
+background_data=[]
+background_label=[]
+foreground_data=[]
+foreground_label=[]
+batch_size=10
+
+for i in range(5000):   #5000*batch_size = 50000 data points
+    images, labels = dataiter.next()
+    for j in range(batch_size):
+        if(classes[labels[j]] in background_classes):
+            img = images[j].tolist()
+            background_data.append(img)
+            background_label.append(labels[j])
+        else:
+            img = images[j].tolist()
+            foreground_data.append(img)
+            foreground_label.append(labels[j])
+            
+foreground_data = torch.tensor(foreground_data)
+foreground_label = torch.tensor(foreground_label)
+background_data = torch.tensor(background_data)
+background_label = torch.tensor(background_label)
+
+
+# In[12]:
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img#.numpy()
+    plt.imshow(np.transpose(npimg, axes = (1, 2, 0)))
+    plt.show()
+
+
+# In[13]:
+
+
+def create_mosaic_img(bg_idx,fg_idx,fg): 
+    """
+      bg_idx : list of indexes of background_data[] to be used as background images in mosaic
+      fg_idx : index of image to be used as foreground image from foreground data
+      fg : at what position/index foreground image has to be stored out of 0-8
+    """
+    image_list=[]
+    j=0
+    for i in range(9):
+        if i != fg:
+            image_list.append(background_data[bg_idx[j]].type("torch.DoubleTensor"))
+            j+=1
+        else: 
+            image_list.append(foreground_data[fg_idx].type("torch.DoubleTensor"))
+            label = foreground_label[fg_idx]  #-7  # minus 7 because our fore ground classes are 7,8,9 but we have to store it as 0,1,2
+    #image_list = np.concatenate(image_list ,axis=0)
+    image_list = torch.stack(image_list) 
+    return image_list,label
+
+
+# In[14]:
+
+
+desired_num = 30000
+mosaic_list_of_images =[]      # list of mosaic images, each mosaic image is saved as list of 9 images
+fore_idx =[]                   # list of indexes at which foreground image is present in a mosaic image i.e from 0 to 9               
+mosaic_label=[]                # label of mosaic image = foreground class present in that mosaic
+for i in range(desired_num):
+    bg_idx = np.random.randint(0,35000,8)
+    fg_idx = np.random.randint(0,15000)
+    fg = np.random.randint(0,9)
+    fore_idx.append(fg)
+    image_list,label = create_mosaic_img(bg_idx,fg_idx,fg)
+    mosaic_list_of_images.append(image_list)
+    mosaic_label.append(label)
+
+
+# In[15]:
+
+
+class MosaicDataset(Dataset):
+    """MosaicDataset dataset."""
+
+    def __init__(self, mosaic_list_of_images, mosaic_label, fore_idx):
+        """
+          Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.mosaic = mosaic_list_of_images
+        self.label = mosaic_label
+        self.fore_idx = fore_idx
+
+    def __len__(self):
+        return len(self.label)
+
+    def __getitem__(self, idx):
+        return self.mosaic[idx] , self.label[idx], self.fore_idx[idx]
+
+batch = 250
+msd = MosaicDataset(mosaic_list_of_images, mosaic_label , fore_idx)
+train_loader = DataLoader( msd,batch_size= batch ,shuffle=True)
+
+
+# In[20]:
+
+
+test_images =[]        #list of mosaic images, each mosaic image is saved as laist of 9 images
+fore_idx_test =[]                   #list of indexes at which foreground image is present in a mosaic image                
+test_label=[]                # label of mosaic image = foreground class present in that mosaic
+for i in range(10000):
+    bg_idx = np.random.randint(0,35000,8)
+    fg_idx = np.random.randint(0,15000)
+    fg = np.random.randint(0,9)
+    fore_idx_test.append(fg)
+    image_list,label = create_mosaic_img(bg_idx,fg_idx,fg)
+    test_images.append(image_list)
+    test_label.append(label)
+
+
+# In[21]:
+
+
+test_data = MosaicDataset(test_images,test_label,fore_idx_test)
+test_loader = DataLoader( test_data,batch_size= batch ,shuffle=False)
+
+
+# In[16]:
+
+
+what = Net()
+what.load_state_dict(torch.load("weight_pretrained1/net_epoch99.pt"))
+what.fc2 = nn.Linear(120,3)
+
+
+# In[17]:
+
+
+class Wherenet(nn.Module):
+    def __init__(self):
+        super(Wherenet,self).__init__()
+        self.helper = Net()
+        self.helper.load_state_dict(torch.load("weight_pretrained1/net_epoch99.pt"))
+        self.helper.fc2 = nn.Linear(120,1)
+    def forward(self,z):
+        x = torch.zeros([batch,9],dtype=torch.float64)
+        y = torch.zeros([batch,3, 32,32], dtype=torch.float64)
+        x,y = x.to("cuda"),y.to("cuda")
+        for i in range(9):
+            x[:,i] = self.helper(z[:,i])[:,0]
+        x = F.softmax(x,dim=1)   # alphas
+    
+        x1 = x[:,0]
+        torch.mul(x1[:,None,None,None],z[:,0])
+
+        for i in range(9):            
+            x1 = x[:,i]          
+            y = y + torch.mul(x1[:,None,None,None],z[:,i])
+        return y , x 
+        
+
+
+# In[18]:
+
+
+where = Wherenet().double()
+where = where.to(device)
+what = what.double()
+what =what.to(device)
+
+
+# In[22]:
+
+
+
+focus_true_pred_true =0
+focus_false_pred_true =0
+focus_true_pred_false =0
+focus_false_pred_false =0
+
+argmax_more_than_half = 0
+argmax_less_than_half =0
+
+cifar_acc = []
+
+col1=[]
+col2=[]
+col3=[]
+col4=[]
+col5=[]
+col6=[]
+col7=[]
+col8=[]
+col9=[]
+col10=[]
+col11=[]
+col12=[]
+col13=[]
+
+criterion = nn.CrossEntropyLoss()
+optimizer_where = optim.SGD(where.parameters(), lr=0.01, momentum=0.9)
+optimizer_what = optim.SGD(what.parameters(), lr=0.01, momentum=0.9)
+
+nos_epochs = 150
+
+train_loss=[]
+test_loss =[]
+train_acc = []
+test_acc = []
+ig = np.random.randint(0,250)
+for epoch in range(nos_epochs):  # loop over the dataset multiple times
+
+    focus_true_pred_true =0
+    focus_false_pred_true =0
+    focus_true_pred_false =0
+    focus_false_pred_false =0
+
+    argmax_more_than_half = 0
+    argmax_less_than_half =0
+
+    running_loss = 0.0
+    cnt=0
+    c = 0
+    iteration = desired_num // batch
+
+    #training data set
+
+    for i, data in  enumerate(train_loader):
+        inputs , labels , fore_idx = data
+        inputs,labels,fore_idx = inputs.to(device),labels.to(device),fore_idx.to(device)
+        # zero the parameter gradients
+        
+        optimizer_what.zero_grad()
+        optimizer_where.zero_grad()
+        
+        
+        avg_inp,alphas = where(inputs)
+        
+        outputs = what(avg_inp)
+        
+        _, predicted = torch.max(outputs.data, 1)
+        
+        
+        # display plots 
+#         if(c==0): 
+#             disp_plot(inputs[ig,:],avg_inp[ig,:],ig,labels[ig].item(),predicted[ig].item(), alphas[ig,:], fore_idx[ig].item())
+#             c+=1
+        
+        loss = criterion(outputs, labels) 
+        loss.backward() 
+        
+        optimizer_what.step()
+        optimizer_where.step() 
+        
+
+        running_loss += loss.item()
+        if cnt % 40 == 39:    # print every 6 mini-batches
+            print('[%d, %5d] loss: %.3f' %(epoch + 1, cnt + 1, running_loss / 40))
+            running_loss = 0.0
+        cnt=cnt+1
+    
+        if epoch % 5 == 4:
+            for j in range (batch):
+                focus = torch.argmax(alphas[j])
+
+                if(alphas[j][focus] >= 0.5):
+                    argmax_more_than_half +=1
+                else:
+                    argmax_less_than_half +=1
+
+                if(focus == fore_idx[j] and predicted[j] == labels[j]):
+                    focus_true_pred_true += 1
+
+                elif(focus != fore_idx[j] and predicted[j] == labels[j]):
+                    focus_false_pred_true +=1
+
+                elif(focus == fore_idx[j] and predicted[j] != labels[j]):
+                    focus_true_pred_false +=1
+
+                elif(focus != fore_idx[j] and predicted[j] != labels[j]):
+                    focus_false_pred_false +=1
+
+    if epoch % 5 == 4:
+        col1.append(epoch)
+        col2.append(argmax_more_than_half)
+        col3.append(argmax_less_than_half)
+        col4.append(focus_true_pred_true)
+        col5.append(focus_false_pred_true)
+        col6.append(focus_true_pred_false)
+        col7.append(focus_false_pred_false)
+
+    #************************************************************************
+        #testing data set  
+        with torch.no_grad():
+            full_batch_true = 0
+            
+            focus_true_pred_true =0
+            focus_false_pred_true =0
+            focus_true_pred_false =0
+            focus_false_pred_false =0
+            argmax_more_than_half = 0
+            argmax_less_than_half =0
+            for data in trainloader:
+                inputs,labels = data
+                
+                inputs,labels = inputs.to(device), labels.to(device)
+                
+                outputs = what(inputs.double())
+                
+                _,predicted = torch.max(outputs.data,1)
+                #print(predicted.cpu().numpy(),"labels",labels.cpu().numpy())
+                full_batch_true+=sum(predicted.cpu().numpy()== labels.cpu().numpy())
+            
+            cifar_acc.append(full_batch_true)
+            print("focibly_true_accuracy: ",full_batch_true)
+            
+            
+            for data in test_loader:
+                inputs, labels , fore_idx = data
+                inputs,labels,fore_idx = inputs.to(device),labels.to(device),fore_idx.to(device) 
+#                 print(inputs.shtorch.save(where.state_dict(),"model_epoch"+str(epoch)+".pt")ape,labels.shape)
+                avg_inp,alphas = where(inputs)
+                outputs = what(avg_inp)
+                _, predicted = torch.max(outputs.data, 1)
+
+                for j in range (batch):
+                    focus = torch.argmax(alphas[j])
+
+                    if(alphas[j][focus] >= 0.5):
+                        argmax_more_than_half +=1
+                    else:
+                        argmax_less_than_half +=1
+
+                    if(focus == fore_idx[j] and predicted[j] == labels[j]):
+                        focus_true_pred_true += 1
+
+                    elif(focus != fore_idx[j] and predicted[j] == labels[j]):
+                        focus_false_pred_true +=1
+
+                    elif(focus == fore_idx[j] and predicted[j] != labels[j]):
+                        focus_true_pred_false +=1
+
+                    elif(focus != fore_idx[j] and predicted[j] != labels[j]):
+                        focus_false_pred_false +=1
+
+        col8.append(argmax_more_than_half)
+        col9.append(argmax_less_than_half)
+        col10.append(focus_true_pred_true)
+        col11.append(focus_false_pred_true)
+        col12.append(focus_true_pred_false)
+        col13.append(focus_false_pred_false)
+        torch.save(where.state_dict(),"weight_pretrained1/where_model_epoch"+str(epoch)+".pt")
+        torch.save(what.state_dict(),"weight_pretrained1/what_model_epoch"+str(epoch)+".pt")
+    
+print('Finished Training')
+torch.save(where.state_dict(),"weight_pretrained1/where_model_epoch"+str(nos_epochs)+".pt")
+torch.save(what.state_dict(),"weight_pretrained1/what_model_epoch"+str(epoch)+".pt")
+
+
+# In[23]:
+
+
+full_train_acc = 0 
+for data in train_loader:
+    inputs,labels,fore_idx = data
+    inputs,labels,fore_idx = inputs.to(device), labels.to(device),fore_idx.to(device)
+    avg_inp,alphas = where(inputs)
+    outputs = what(avg_inp)
+    _,predicted = torch.max(outputs.data,1)
+    #print(predicted.cpu().numpy(),"labels",labels.cpu().numpy())
+    full_train_acc+=sum(predicted.cpu().numpy()== labels.cpu().numpy())
+print("mosaic_data_training_accuracy :",full_train_acc/30000)
+
+
+# In[24]:
+
+
+full_test_acc = 0 
+for data in test_loader:
+    inputs,labels,fore_idx = data
+    inputs,labels,fore_idx = inputs.to(device), labels.to(device),fore_idx.to(device)
+    avg_inp,alphas = where(inputs)
+    outputs = what(avg_inp)
+    _,predicted = torch.max(outputs.data,1)
+    #print(predicted.cpu().numpy(),"labels",labels.cpu().numpy())
+    full_test_acc+=sum(predicted.cpu().numpy()== labels.cpu().numpy())
+print("mosaic_data_test_accuracy :",full_test_acc/10000)
+
+
+# In[25]:
+
+
+columns = ["epochs", "argmax > 0.5" ,"argmax < 0.5", "focus_true_pred_true", "focus_false_pred_true", "focus_true_pred_false", "focus_false_pred_false" ]
+df_train = pd.DataFrame()
+df_test = pd.DataFrame()
+df_train[columns[0]] = col1
+df_train[columns[1]] = col2
+df_train[columns[2]] = col3
+df_train[columns[3]] = col4
+df_train[columns[4]] = col5
+df_train[columns[5]] = col6
+df_train[columns[6]] = col7
+df_test[columns[0]] = col1
+df_test[columns[1]] = col8
+df_test[columns[2]] = col9
+df_test[columns[3]] = col10
+df_test[columns[4]] = col11
+df_test[columns[5]] = col12
+df_test[columns[6]] = col13
+
+
+# In[26]:
+
+
+df_train.to_csv("weight_pretrained1/train_results.csv",index=False)
+df_test.to_csv("weight_pretrained1/test_results.csv",index=False)
+
+
+# In[ ]:
+
+
+
+
