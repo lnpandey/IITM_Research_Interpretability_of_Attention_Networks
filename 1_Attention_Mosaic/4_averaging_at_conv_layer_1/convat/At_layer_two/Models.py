3ebfa35aa970b81c3bece82ecf548344cccc19a4
VASHISHT-RAHUL <cs18d006@smail.iitm.ac.in>
2020-03-11 13:04:50 +0530

    file reorganisation

--- 1_Attention_Mosaic/4_averaging_at_conv_layer_1/convat/At_layer_two/Models.py
+++ 1_Attention_Mosaic/4_averaging_at_conv_layer_1/convat/At_layer_two/Models.py
@@ -20,22 +20,23 @@ class Module1(nn.Module):
     
     self.conv1 = nn.Conv2d(self.input, 8, 5)
     self.pool1 = nn.MaxPool2d(2, 2)
-    self.conv2 = nn.Conv2d(8,8,5)
+    self.conv2 = nn.Conv2d(8,16,5,padding=3)
     #self.pool2 = nn.MaxPool2d(2,2)
-    self.conv3 = nn.Conv2d(8,16,5)
+    self.conv3 = nn.Conv2d(16,16,5)
     self.conv4 = nn.Conv2d(16,16,3)
-    self.linear1 = nn.Linear(16*4*4,self.output)
+    self.linear1 = nn.Linear(16*10*10,self.output)
 
   def forward(self,x):
     x = F.relu(self.conv1(x))
-    x1 = x
     x = self.pool1(x)
     x = F.relu(self.conv2(x))
+    #print(x.shape)
+    x1 = x
     #x = self.pool2(x)
     x = F.relu(self.conv3(x))
     x = F.relu(self.conv4(x))
     #print("flag 1",x.shape)
-    x = x.view(-1,16*4*4)
+    x = x.view(-1,16*10*10)
     x = self.linear1(x)
     
     
@@ -57,21 +58,22 @@ class Focus_Module(nn.Module):
   def forward(self, z):
     batch = z.shape[0]
     x = torch.zeros([batch,9],dtype=torch.float64)
-    y = torch.zeros([batch,8, 28,28], dtype=torch.float64)
+    y = torch.zeros([batch,16, 16,16], dtype=torch.float64)
     x,y = x.to(device),y.to(device)
     z11 = []
     for i in range(9):
-      ll,z1 = self.helper(z[:,i])
-      x[:,0] = ll[:,0]
-      z11.append(z1)
+        ll,z1 = self.helper(z[:,i])
+        x[:,i] = ll[:,0]
+        z11.append(z1)
+    print(x)
     x = F.softmax(x,dim=1)   # alphas
     #print(np.shape(z11))
     x1 = x[:,0]
     torch.mul(x1[:,None,None,None],z11[0])
 
-    for i in range(9):            
-      x1 = x[:,i]          
-      y = y + torch.mul(x1[:,None,None,None],z11[i])
+    for i in range(9):
+        x1 = x[:,i]          
+        y = y + torch.mul(x1[:,None,None,None],z11[i])
     return y , x 
   
   def helper(self,x):
@@ -87,13 +89,13 @@ class Classification_Module(nn.Module):
     super(Classification_Module,self).__init__()
     self.input = inp
     self.output = out
-    self.conv1 = nn.Conv2d(self.input, 8, 5)
+    self.conv1 = nn.Conv2d(self.input, 32, 5,padding=3)
     self.pool1 = nn.MaxPool2d(2, 2)
-    self.conv2 = nn.Conv2d(8,8,5)
+    self.conv2 = nn.Conv2d(32,32,5,padding =1)
     #self.pool2 = nn.MaxPool2d(2,2)
-    self.conv3 = nn.Conv2d(8,16,5)
-    self.conv4 = nn.Conv2d(16,16,3)
-    self.linear1 = nn.Linear(16*2*2,self.output)
+    self.conv3 = nn.Conv2d(32,64,5,padding =1)
+    self.conv4 = nn.Conv2d(64,64,3)
+    self.linear1 = nn.Linear(64*3*3,self.output)
   def forward(self,x):
     x = F.relu(self.conv1(x))
     x = self.pool1(x)
@@ -102,9 +104,10 @@ class Classification_Module(nn.Module):
     x = F.relu(self.conv3(x))
     x = F.relu(self.conv4(x))
     #print("flag 1",x.shape)
-    x = x.view(-1,16*2*2)
+    x = x.view(-1,64*3*3)
     x = self.linear1(x)
     return x 
+
 
 
     
