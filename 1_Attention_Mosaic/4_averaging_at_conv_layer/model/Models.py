b2a61beb9ecaf1331178d52964f8da24b9a39214
VASHISHT-RAHUL <CS18D006@smail.iitm.ac.in>
2020-05-28 18:11:24 +0530

    adding files

--- 1_Attention_Mosaic/4_averaging_at_conv_layer/model/Models.py
+++ 1_Attention_Mosaic/4_averaging_at_conv_layer/model/Models.py
@@ -107,7 +107,84 @@ class Module3(nn.Module):
     x = F.relu(self.fc3(x))
     x = self.fc4(x)
     return x,x1
+
+
+class Module4(nn.Module):
+  
+  '''
+  Input :  32X32X3 image
+  Convolutional Network with 4 Conv layers 
+  '''
+  def __init__(self,inp,out):
+    super(Module4,self).__init__()
+    self.input = inp
+    self.output = out
+    
+    self.conv1 = nn.Conv2d(self.input, 6, 5)
+    self.pool = nn.MaxPool2d(2, 2)
+    self.conv2 = nn.Conv2d(6, 12, 5)
+    self.conv3 = nn.Conv2d(12,20,5)
+    self.fc1 = nn.Linear(20 * 6 * 6, 120)
+    self.fc2 = nn.Linear(120, 84)
+    self.fc3 = nn.Linear(84, 10)
+    self.fc4 = nn.Linear(10,self.output)
+
+  def forward(self,x):
+    x = self.conv1(x)
+    x1 = F.tanh(x)
+    x = self.pool(F.relu(x))
+    x = F.relu(self.conv2(x))
+    x = F.relu(self.conv3(x))
+    #print("Flag1",x1.shape)
+    
+    x = x.view(-1, 20* 6 * 6)
+    x = F.relu(self.fc1(x))
+    x = F.relu(self.fc2(x))
+    x = F.relu(self.fc3(x))
+    x = self.fc4(x)
+    return x,x1
 		
+
+class Focus_Module4(nn.Module):
+  '''
+  Focus Network uses Module 4
+  '''
+  def __init__(self,inp,out):
+    super(Focus_Module4, self).__init__()
+    self.input = inp
+    self.output = out
+    self.module = Module4(self.input,self.output)
+
+  def forward(self, z):
+    #print("flag1",z.shape)
+    #print("flag2",z[:,0].shape)
+    batch = z.shape[0]
+    x = torch.zeros([batch,9],dtype=torch.float64)
+    y = torch.zeros([batch,6, 28,28], dtype=torch.float64)
+    feature = torch.zeros([batch,9,6,28,28],dtype=torch.float64)
+    x,y = x.to(device),y.to(device)
+    feature = feature.to(device)
+    for i in range(9):
+      alp,ftr= self.helper(z[:,i])
+      #print("flag3",ftr.shape)
+      x[:,i] = alp[:,0]
+      feature[:,i] = ftr
+      
+      
+    x = F.softmax(x,dim=1)   # alphas
+    
+    #x1 = x[:,0]
+    #torch.mul(x1[:,None,None,None],z[:,0])
+
+    for i in range(9):            
+      x1 = x[:,i]          
+      y = y + torch.mul(x1[:,None,None,None],feature[:,i])
+    return y , x 
+  
+  def helper(self,x):
+    x,features = self.module(x)
+    #print(x.shape)
+    return x,features
 
 
 
@@ -126,8 +203,8 @@ class Focus_Module3(nn.Module):
     #print("flag2",z[:,0].shape)
     batch = z.shape[0]
     x = torch.zeros([batch,9],dtype=torch.float64)
-    y = torch.zeros([batch,12, 10,10], dtype=torch.float64)
-    feature = torch.zeros([batch,9,12,10,10],dtype=torch.float64)
+    y = torch.zeros([batch,9, 6,10], dtype=torch.float64)
+    feature = torch.zeros([batch,9,6,10,10],dtype=torch.float64)
     x,y = x.to(device),y.to(device)
     feature = feature.to(device)
     for i in range(9):
@@ -299,6 +376,31 @@ class Classification_Module3(nn.Module):
     x = self.fc4(x)
     return x
 
+class Classification_Module4(nn.Module):
+  '''
+  Classification Network  
+  '''
+  def __init__(self,inp,out):
+    super(Classification_Module4,self).__init__()
+    self.input = inp
+    self.output = out
+    self.pool = nn.MaxPool2d(2, 2)
+    self.conv1 = nn.Conv2d(self.input, 12, 5)
+    self.conv2 = nn.Conv2d(12,20,5)
+    self.fc1 = nn.Linear(20 *8  *8 , 120)
+    self.fc2 = nn.Linear(120, 84)
+    self.fc3 = nn.Linear(84, 10)
+    self.fc4 = nn.Linear(10,self.output)
+  def forward(self,x):
+    x = F.relu(self.pool(self.conv1(x)))
+    x= F.relu(self.conv2(x))
+    #print("flag1",x.shape)
+    x = x.view(-1,20*8*8)
+    x = F.relu(self.fc1(x))
+    x = F.relu(self.fc2(x))
+    x = F.relu(self.fc3(x))
+    x = self.fc4(x)
+    return x
 
 
     
