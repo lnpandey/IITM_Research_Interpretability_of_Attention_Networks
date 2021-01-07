66eda5d09cdb0204146701196e33c23b7fcd8fe6
VASHISHT-RAHUL <CS18D006@smail.iitm.ac.in>
2021-01-07 13:56:18 +0530

    adding files

--- 13_COCO_Dataset/model/models.py
+++ 13_COCO_Dataset/model/models.py
@@ -144,3 +144,155 @@ class Decoder(nn.Module):
         output = F.softmax(output, dim = 1)
         
         return output, hidden, attn_weights
+
+
+# Show Attend and Tell model
+class ShowAttendTell(nn.Module):
+    
+    def __init__(self,encoder,decoder,vocabulary,teacher_forcing_ratio, batch_size=batch_size,):
+        super(ShowAttendTell,self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.voc = vocabulary
+        self.batch_size = batch_size
+        self.enc_optimizer = optim.Adam(self.encoder.parameters(),lr=0.00001)
+        self.dec_optimizer = optim.Adam(self.decoder.parameters(),lr=0.001)
+        self.teacher_forcing_ratio = teacher_forcing_ratio
+        self.print_every = 400
+        
+    def update_hyperparam(self,encoder_lr,decoder_lr,teacher_forcing_ratio):
+        self.encoder_lr = encoder_lr
+        self.decoder_lr = decoder_lr
+        self.enc_optimizer = optim.Adam(self.encoder.parameters(),lr=self.encoder_lr)
+        self.dec_optimizer = optim.Adam(self.decoder.parameters(),lr=self.decoder_lr)
+        self.teacher_forcing_ratio = teacher_forcing_ratio
+ 
+        
+    def load(self,encoder_path = 'Save/VGG_encoder_10.pt',decoder_path='Save/VGG_decoder_10.pt'):
+        self.encoder = torch.load(encoder_path)
+        self.decoder = torch.load(decoder_path)
+        #self.encoder.load_state_dict(torch.load(encoder_path))
+        #self.decoder.load_state_dict(torch.load(decoder_path))
+        
+    def train_epoch(self,dataloader,clip=5):
+        
+        total_loss = 0
+        start_iteration = 1
+        print_loss = 0
+        iteration = 1
+        for data in dataloader:
+            features, targets, mask, max_length,_ = data
+            use_teacher_forcing = True if random.random() < self.teacher_forcing_ratio else False
+            loss = self.train_iter(features,targets,mask,max_length,use_teacher_forcing,clip)
+            print_loss += loss
+            total_loss += loss
+
+        # Print progress
+            if iteration % self.print_every == 0:
+                print_loss_avg = print_loss / self.print_every
+                print("Iteration: {}; Percent complete: {:.1f}%; Average loss: {:.4f}".
+                format(iteration, iteration / len(dataloader) * 100, print_loss_avg))
+                print_loss = 0
+            
+            iteration += 1
+            
+        return total_loss/len(dataloader)
+            
+        
+    def train_iter(self,input_variable, target_variable, mask,max_target_len,use_teacher_forcing,clip=5):
+        
+        self.enc_optimizer.zero_grad()
+        self.dec_optimizer.zero_grad()
+        
+        loss = 0
+        print_losses = []
+        n_totals = 0
+        
+        input_variable = input_variable.to(device)
+        target_variable = target_variable.to(device)
+        mask = mask.byte().to(device)
+        
+        enc_output = self.encoder(input_variable)
+        decoder_input = torch.LongTensor([[SOS_token for _ in range(batch_size)]])
+        decoder_input = decoder_input.to(device)
+        decoder_hidden = (torch.zeros(1, batch_size, self.decoder.hidden_size).to(device),
+                  torch.zeros(1, batch_size, self.decoder.hidden_size).to(device))
+        
+        # Forward batch of sequences through decoder one time step at a time
+        if use_teacher_forcing:
+            for t in range(max_target_len):
+                decoder_output, decoder_hidden,_ = self.decoder(decoder_input, decoder_hidden,enc_output)
+                decoder_input = target_variable[t].view(1, -1)
+                mask_loss, nTotal = maskNLLLoss(decoder_output.unsqueeze(0), target_variable[t], mask[t])
+                loss += mask_loss
+                print_losses.append(mask_loss.item() * nTotal)
+                n_totals += nTotal
+        else:
+            for t in range(max_target_len):
+                #print('decoder hidden in sampling :',decoder_hidden.size())
+                decoder_output, decoder_hidden,_ = self.decoder(decoder_input, decoder_hidden,enc_output )
+                # No teacher forcing: next input is decoder's own current output
+                _, topi = decoder_output.squeeze(0).topk(1)
+                decoder_input = torch.LongTensor([[topi[i][0] for i in range(self.batch_size)]])
+                decoder_input = decoder_input.to(device)
+                #print('decoder input size in training :',decoder_input.size())
+                # Calculate and accumulate loss
+                mask_loss, nTotal = maskNLLLoss(decoder_output, target_variable[t], mask[t])
+                loss += mask_loss
+                print_losses.append(mask_loss.item() * nTotal)
+                n_totals += nTotal
+
+        # Perform backpropatation
+        loss.backward()
+
+        # Clip gradients: gradients are modified in place
+        _ = nn.utils.clip_grad_norm_(self.encoder.parameters(), clip)
+        _ = nn.utils.clip_grad_norm_(self.decoder.parameters(), clip)
+
+        # Adjust model weights
+        self.enc_optimizer.step()
+        self.dec_optimizer.step()
+
+        return sum(print_losses) / n_totals
+            
+        
+    def Greedy_Decoding(self,features,max_length=15):
+        enc_output = self.encoder(features)
+        batch_size = features.size()[0]
+        decoder_hidden = (torch.zeros(1, batch_size, self.decoder.hidden_size).to(device),
+                          torch.zeros(1, batch_size, self.decoder.hidden_size).to(device))
+        
+        decoder_input = torch.LongTensor([[SOS_token for _ in range(batch_size)]]).to(device)
+        #print('Initial size',decoder_input.size())
+        caption = []
+        attn_weights = []
+        for _ in range(max_length):
+            decoder_output, decoder_hidden, attn_values = self.decoder(decoder_input, decoder_hidden, enc_output)
+            #print(attn_values.shape)
+
+            #print('decoder hidden size :',decoder_hidden.size())
+            #print('decoder output size :',decoder_output.size())
+            _, topi = decoder_output.topk(1)
+            #print(topi)
+            #print('topi size',topi.size())
+            decoder_input = topi.permute(1,0).to(device)
+            caption.append(topi.squeeze(1).cpu())
+            attn_weights.append(attn_values[:,:,0].cpu())
+        caption = torch.stack(caption,0).permute(1,0)
+        attn_weights= torch.stack(attn_weights,0)
+        #print(attn_weights.size())
+
+        caps_text = []
+        for dta in caption:
+            tmp = []
+            for token in dta:
+                if token.item() not in self.voc.index2word.keys() or token.item()==2:
+                    pass
+                else:
+                    tmp.append(self.voc.index2word[token.item()])
+            tmp = ' '.join(x for x in tmp)
+            caps_text.append(tmp)
+        return caption,caps_text,attn_weights
+    
+
+
